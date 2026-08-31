e9519fbe898fa126db173e05c70fa86fb3843323
Sujaygouda R Patil <sujaygoudarpatilweb3@gmail.com>
2026-08-31 18:32:40 +0530

    fontend-masters: gallery project implementation using 3rd party libraries

---
+++ projects/Frontend-Masters/gallery-using-libraries/gallery.js
@@ -0,0 +1,42 @@
+
+// import { animate } from "motion/mini"
+// import { spring } from "motion"
+
+new Swiper(".swiper", {
+    speed: 400,
+    spaceBetween: 100,
+    // slidesPerView: 3,
+    effect: 'slide',
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+});
+
+// navigation physics:
+
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector(".brand");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, "mousedown touchstart").start(e => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+});
+
+listen(document, "mouseup touchend").start(() => {
+    spring({
+        from: ballXY.get(),
+        velocity: ballXY.getVelocity(),
+        to: { x: 0, y: 0 },
+        stiffness: 200
+        // mass: 1,
+        // damping: 10
+    }).start(ballXY);
+});
